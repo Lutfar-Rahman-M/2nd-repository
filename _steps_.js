593e74c7a3ed2e05fea3d10289d94bf2984c4e1d
Lutfar-Rahman-M <lutfarbtc@gmail.com>
2023-05-21 15:02:37 +0600

    First Commit

---
+++ _steps_.js
@@ -0,0 +1,12 @@
+/****
+ * step 1: create a github account 
+ * step 2: login to yout git account
+ * step 3: create a repository on git
+ * step 4: connect your repository to your local projects
+ * step 5: initial git ripo to your local project
+ * step 6: add your project to git ripo
+ * step 7: 
+ * 
+ * 
+ * 
+ */
